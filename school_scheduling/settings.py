5c7dcdef9a25ee5bcdd48c047e8727ec65a640ec
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-21 19:12:30 +0100

    Move SECRET_KEY and DATABASE_URL to environment variables

--- school_scheduling/settings.py
+++ school_scheduling/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(mf0$)(awr_hlby(8*@m5q2a4cpr9qfv)30*3(-8ur5%m3d+yq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
