1204caca080526aed3163f9ed90c099e53de7998
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-23 18:56:34 +0100

    Add stage and subject models and regsiter them in admin.py

--- timetabling/models.py
+++ timetabling/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Stage(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+    
+
+class Subject(models.Model):
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ('stage', 'name')
+    
+    def __str__(self):
+        return f"{self.name} ({self.stage})"
