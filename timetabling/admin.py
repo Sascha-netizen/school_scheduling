73bf62c46f3be59c74fc8fd9560399ba360b0a5f
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-23 19:34:31 +0100

    Add room model and register it in admin.py

--- timetabling/admin.py
+++ timetabling/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Stage, Subject
+from .models import Stage, Subject, Room
 # Register your models here.
 
 admin.site.register(Stage)
 admin.site.register(Subject)
+admin.site.register(Room)
--- timetabling/models.py
+++ timetabling/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Stage(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
@@ -13,6 +14,18 @@ class Stage(models.Model):
     
 
 class Subject(models.Model):
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ('stage', 'name')
+    
+    def __str__(self):
+        return f"{self.name} ({self.stage})"
+
+
+class Room(models.Model):
     stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
