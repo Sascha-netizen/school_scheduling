79a06f612cd5b5f79abcfc23aa1236331ecbcad5
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-25 20:38:23 +0100

    Add first app-level view, wire up urls and create templates

--- timetabling/views.py
+++ timetabling/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def my_timetable(request):
-    return HttpResponse("Hello, timetable!")
+def home(request):
+    return render(request, "timetabling/index.html")
