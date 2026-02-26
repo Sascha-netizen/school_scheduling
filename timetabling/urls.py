32b4d6a3021723cc205828a54ab2c826a5451241
Sascha Klement <sascha.r.klement@gmail.com>
2026-02-26 21:15:25 +0100

    Add roles-based access

--- timetabling/urls.py
+++ timetabling/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name='home'),
+    path('create_schedule/', views.create_schedule, name='create_schedule'),
+    path('view_schedule/', views.view_schedule, name='view_schedule'),
 ]
--- timetabling/views.py
+++ timetabling/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -14,3 +15,15 @@ def home(request):
         'is_secretary': is_secretary,
         'is_teacher': is_teacher,
     })
+
+@login_required
+def create_schedule(request):
+    if not request.user.groups.filter(name='Secretaries').exists():
+        return redirect('home')
+    return render(request, "timetabling/create_schedule.html")
+
+@login_required
+def view_schedule(request):
+    if not request.user.groups.filter(name='Teachers').exists():
+        return redirect('home')
+    return render(request, "timetabling/view_schedule.html")
